3a1fe048df1c58925dbe7553a76f7a60f7f4576b
Sami Konzo <samikonzo@gmail.com>
2018-01-11 18:53:27 +0300

    :commit

--- public/script.js
+++ public/script.js
@@ -5,8 +5,19 @@ document.addEventListener("DOMContentLoaded", ready);
 
 function ready(){
 	slider = document.querySelector('.slider')
+	slider.count = 0;
 	tmpl = document.getElementById('slider-item__template').innerHTML
-	createSlide()
+	
+	//show slider
+	setTimeout(function(){
+		slider.style.left = '0px';
+		slider.style.opacity = 1;
+	}, 10)
+
+	//add first imgs
+	for(var i = 0; i < 10; i++){
+		createSlide()
+	}
 }
 
 
@@ -18,30 +29,33 @@ function createSlide(){
 	var img = el.querySelector('img')
 	var msk = el.querySelector('div')
 
-	setTimeout(function f(){ 
-		getImg(img,msk)
-		
-		setTimeout(f, 1000)
-	}, 0)
+	img.el = el;
 
+	getImg(img, msk)
 	slider.appendChild(el)
 }
 
 function getImg(img, msk){
 	var xhr = new XMLHttpRequest()
-	xhr.open('GET', 'nextImg', true)
+	xhr.open('POST', 'nextImg', true)
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 	xhr.onload = function(e){
 		if(this.status == 200){
+			l(this.response)
 			img.onload = function(){
 				msk.classList.add('slider-item__mask--hidden')
 			}
 
 			img.src = this.response
+
 		} else {
-			l('status :', this.status)
+			l('status :', this.statusText)
+			img.el.remove()
 		}
 	}
 
-	xhr.send()
+	var body = 'count='+ encodeURIComponent(slider.count++)
+
+	xhr.send(body)
 }
--- serverApp.js
+++ serverApp.js
@@ -1,11 +1,13 @@
+global.l = console.log
+
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const fs = require('fs')
 const path = require('path')
 const ROOT = __dirname + '/public'
 const IMAGES = ROOT + '/images/'
 
-var counter = 0;
 var imageFiles = [];
 fs.readdir(IMAGES, (err, files) => {
 	if(err) {
@@ -18,23 +20,26 @@ fs.readdir(IMAGES, (err, files) => {
 	})
 })
 
-
-
-
-
+//static files
 app.use(express.static(__dirname + '/public'))
 
-app.get('/nextImg', (req, res) => {
-	console.log('next img needed')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-	if(counter > imageFiles.length - 1){
-		counter = 0;
-	}
-
-	var currentFile = imageFiles[counter++]
-
-	res.end(/images/ + currentFile)
-	
-})
+// parse application/json
+app.use(bodyParser.json())
 
 app.listen(3000)
+
+app.post('/nextImg', (req, res) => {
+	var yetCount = req.body.count
+
+	if(imageFiles[yetCount]){
+		res.writeHead(200)
+		res.end(/images/ + imageFiles[yetCount])
+	} else {
+		res.writeHead(204)
+		res.write('no images else')
+		res.end('')
+	}
+})
